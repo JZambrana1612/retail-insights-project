78fb48ca57d2b04308d073a85bfd89cf47f79338
Jeremy G <zambrana.1612@gmail.com>
2025-07-04 13:15:45 -0400

    Update retail_sales_table_setup.sql

--- sql/retail_sales_table_setup.sql
+++ sql/retail_sales_table_setup.sql
@@ -18,15 +18,3 @@ CREATE TABLE retail_sales (
     price_per_unit DECIMAL(10, 2),
     total_amount DECIMAL(10, 2)
 );
-
--- Load data from CSV file (update the path as needed)
--- NOTE: Ensure 'local_infile' is enabled in MySQL for this to work
-
--- LOAD DATA INFILE '/var/lib/mysql-files/retail_sales.csv'
--- INTO TABLE retail_sales
--- FIELDS TERMINATED BY ',' 
--- ENCLOSED BY '"'
--- LINES TERMINATED BY '\n'
--- IGNORE 1 LINES
--- (transaction_id, @sale_date_raw, customer_id, gender, age, product_category, quantity, price_per_unit, total_amount)
--- SET sale_date = STR_TO_DATE(@sale_date_raw, '%Y-%m-%d %H:%i:%s.%f');
